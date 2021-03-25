7dbc21d0ea6c4940e7b4b864db19def0dbc2d153
Mathew Peachey <mathew.peachey@live.com>
2021-03-25 23:55:40 +1300

    Favico needs hosting config

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask_cors import CORS
 
 app = Flask(__name__, static_url_path='/static')
+app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.ico'))
