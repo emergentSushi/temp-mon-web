bab66d139dd368b17d5dc7a179f0a1d61756594c
Mathew Peachey <mathew.peachey@live.com>
2021-03-26 21:49:29 +1300

    Timezone adjustments so we can host in utc

--- app/data.py
+++ app/data.py
@@ -2,8 +2,11 @@ from app import app
 from flask import render_template, jsonify
 from datetime import timedelta, datetime
 from flask_cors import cross_origin
+from dateutil import tz
 
 import sqlite3
+
+nzst = tz.gettz('Pacific/Auckland')
 
 @app.route('/')
 def index():
@@ -15,8 +18,8 @@ def data():
 	con = sqlite3.connect('data/grid_data.db')
 	con.row_factory = sqlite3.Row
 	cur = con.cursor()
-
-	from_query = datetime.now() - timedelta(hours=12)
+	
+	from_query = datetime.now().astimezone(nzst) - timedelta(hours=12)
 	
 	rows = cur.execute(f'SELECT * FROM data WHERE timestamp > "{from_query.isoformat()}" ORDER BY timestamp DESC')
 	ret = [dict(ix) for ix in rows]
